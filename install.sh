22cdd3f09f74bc48f5edd30b37e76c11ee045062
Tim Walsh <timothyryanwalsh@gmail.com>
2018-04-18 13:43:11 -0400

    Update script and add test script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ### Bitcurator setup script
-# Tim Walsh - Dec 2017
+# Tim Walsh - April 2018
 # Run as sudo
 
 # Install PyQt4
-echo "Installing PyQt4..."
-sudo apt-get install python-qt4
+echo "Installing PyQt5..."
+pip3 install pyqt5
 
 # Force Siegfried to make single identifications
 roy build -multi 0
@@ -14,45 +14,39 @@ roy build -multi 0
 # Download and install CCA Disk Image Processor
 cd /home/bcadmin
 echo "Installing Disk Image Processor..."
-git clone https://github.com/timothyryanwalsh/cca-diskimageprocessor
-cd /home/bcadmin/cca-diskimageprocessor
-sudo chmod u+x install.sh
-sudo ./install.sh
+git clone https://github.com/CCA-Public/diskimageprocessor
+cd /home/bcadmin/diskimageprocessor
+chmod u+x install.sh
+./install.sh
 
 # Download and install CCA Folder Processor
 cd /home/bcadmin
 echo "Installing Folder Processor..."
-git clone https://github.com/timothyryanwalsh/cca-folderprocessor
-cd /home/bcadmin/cca-folderprocessor
-sudo chmod u+x install.sh
-sudo ./install.sh
+git clone https://github.com/CCA-Public/folderprocessor
+cd /home/bcadmin/folderprocessor
+chmod u+x install.sh
+./install.sh
 
 # Download and install CCA SIP Creator
 cd /home/bcadmin
 echo "Installing SIP Creator..."
-git clone https://github.com/timothyryanwalsh/cca-sipcreator
-cd /home/bcadmin/cca-sipcreator
-sudo chmod u+x install.sh
-sudo ./install.sh
-
-# Download and install DFXML Reader
-cd /home/bcadmin
-echo "Installing DFXML Reader"
-git clone https://github.com/timothyryanwalsh/dfxml-reader
-cd /home/bcadmin/dfxml-reader
-sudo chmod u+x install.sh
-sudo ./install.sh
+git clone https://github.com/CCA-Public/sipcreator
+cd /home/bcadmin/sipcreator
+chmod u+x install.sh
+./install.sh
 
 # Create /mnt/diskid/ directory for processing UDF and HFS disks with Disk Image Processor
-sudo mkdir /mnt/diskid
+echo "Creating /mnt/diskid..."
+mkdir /mnt/diskid
 
 # Cleanup folders
 echo "Cleaning up folders..."
-sudo rm -rf /home/bcadmin/brunnhilde-gui
-sudo rm -rf /home/bcadmin/cca-diskimageprocessor
-sudo rm -rf /home/bcadmin/cca-folderprocessor
-sudo rm -rf /home/bcadmin/dfxml-reader
-sudo rm -rf /home/bcadmin/cca-sipcreator
+rm -rf /home/bcadmin/diskimageprocessor
+rm -rf /home/bcadmin/folderprocessor
+rm -rf /home/bcadmin/sipcreator
+
+# Fix permissions for launchers
+chmod a+x -R "/home/bcadmin/Desktop/CCA Tools"
 
 # Echo done
 echo "Finished! CCA Tools installed in /home/bcadmin/Desktop/CCA Tools."
---
+++ install_pyqt5_test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# bitcurator setup script for testing pyqt5 cca tools
+# run as sudo
+
+# general requirements
+pip3 install pyqt5
+roy build -multi 0
+
+# Download and install CCA Disk Image Processor
+cd /home/bcadmin
+echo "Installing Disk Image Processor..."
+git clone https://github.com/timothyryanwalsh/cca-diskimageprocessor
+cd /home/bcadmin/cca-diskimageprocessor
+git checkout dev/pyqt5
+chmod u+x install.sh
+./install.sh
+
+# Download and install CCA Folder Processor
+cd /home/bcadmin
+echo "Installing Folder Processor..."
+git clone https://github.com/timothyryanwalsh/cca-folderprocessor
+cd /home/bcadmin/cca-folderprocessor
+git checkout dev/pyqt5
+chmod u+x install.sh
+./install.sh
+
+# Download and install CCA SIP Creator
+cd /home/bcadmin
+echo "Installing SIP Creator..."
+git clone https://github.com/timothyryanwalsh/cca-sipcreator
+cd /home/bcadmin/cca-sipcreator
+git checkout dev/pyqt5
+chmod u+x install.sh
+./install.sh
+
+# Create /mnt/diskid/ directory for processing UDF and HFS disks with Disk Image Processor
+mkdir /mnt/diskid
+
+# Cleanup folders
+echo "Cleaning up folders..."
+rm -rf /home/bcadmin/brunnhilde-gui
+rm -rf /home/bcadmin/cca-diskimageprocessor
+rm -rf /home/bcadmin/cca-folderprocessor
+rm -rf /home/bcadmin/dfxml-reader
+rm -rf /home/bcadmin/cca-sipcreator
+
+# Fix permissions for launchers
+chmod a+x -R "/home/bcadmin/Desktop/CCA Tools"
+
+# Echo done
+echo "Finished! CCA Tools installed in /home/bcadmin/Desktop/CCA Tools."
